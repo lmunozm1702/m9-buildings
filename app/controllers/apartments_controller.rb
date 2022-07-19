baaf7b86c50df533daaa407ebfb2cf12d146ec53
lmunozm1702 <53025062+lmunozm1702@users.noreply.github.com>
2022-07-19 15:49:06 -0400

    + nested routes

--- app/controllers/apartments_controller.rb
+++ app/controllers/apartments_controller.rb
@@ -1,31 +1,65 @@
 class ApartmentsController < ApplicationController
     def index
-        @apartments = Apartment.all
+        @building = Building.find params[:building_id]
+        @apartments = @building.apartments
     end
 
     def new 
-        @apartment = Apartment.new 
-        @buildings = Building.all
+        @building = Building.find params[:building_id]
+        @apartment = Apartment.new
     end 
     
     def create
+        @building = Building.find params[:building_id]
         @apartment = Apartment.new(apartment_params) # Strong parameters, previene ataques de mass assignment
+        
+        @apartment.building = @building #Asignamos el building, ya no es necesario pasarlo en el formulario, ahora viene en la url
+        
         respond_to do |format|
             if @apartment.save
-                format.html { redirect_to @apartment, notice: 'Apartment was successfully created.' }
+                format.html { redirect_to building_apartment_path(@building, @apartment), 
+                                            notice: 'Apartment was successfully created.' }
             else
                 format.html { render :new }
-            end
+            end                
         end
     end
 
     def show
+        @building = Building.find params[:building_id]
         @apartment = Apartment.find(params[:id])
+    end
+
+    def update
+        @building = Building.find params[:building_id]
+        @apartment = Apartment.find params[:id]
+        respond_to do |format|
+            if @apartment.update(apartment_params.merge(building: @building))#Se añade el building que se obtuvo en la llamada a find_building
+                format.html { redirect_to building_apartment_path(@building, @apartment), 
+                                            notice: 'Apartment was successfully updated.' }
+            else
+                format.html { render :edit }
+            end
+        end
+    end
+
+    def destroy
+        @apartment = Apartment.find params[:id]
+        @apartment.destroy
+        respond_to do |format|
+        format.html { redirect_to building_apartments_url, 
+                    notice: 'Apartment was successfully destroyed.' }
+        end
+    end             
+
+    def edit
+        @building = Building.find params[:building_id]
+        @apartment = Apartment.find params[:id]
     end
     
     private
         def apartment_params
-            params.require(:apartment).permit(:number, :building_id)
+            params.require(:apartment).permit(:number)
         end
 end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  resources :apartments
+
+  #Apartments exists INTO a building:
+  resources :buildings do
+    resources :apartments
+  end
   # Defines the root path route ("/")
   # root "articles#index"
 end
